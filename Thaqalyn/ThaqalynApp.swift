e0559400c5b173f8ef88e19c299937155fded811
Imran.Ali <ali.muhammadimran@gmail.com>
2025-07-31 14:40:57 -0500

    Initial Commit

---
+++ Thaqalyn/ThaqalynApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaqalynApp.swift
+//  Thaqalyn
+//
+//  Created by Imran Ali on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ThaqalynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
